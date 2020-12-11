a8f53de5474ba0f19336919b520bf0d8af2a9a92
doyoon <ian661087@gmail.com>
2020-12-12 01:36:27 +0900

    map

--- map_f/map.c
+++ map_f/map.c
@@ -19,30 +19,14 @@ void set_up()
 	initscr();
 	noecho();
 	crmode();
-	/*
-	char v = '|';
-	char h = '_';
+
 	move(0,0);
-	vline(v, 20);
-	hline(h, 30);
-	move(4,6);
-	hline(h, 4);
-	vline(v, 2);
-	
-	move(4,9);
-	vline(v, 2);
-	move(4,11);
-	hline(h, 2);
-	move(4,12);
-	vline(v, 2);
-	*/
-	move(0,0);
-	vline('*',20);
-	hline('*',30);
-	move(0,30);
-	vline('*',20);
-	move(20,0);
-	hline('*',31);
+	vline('*',25);
+	hline('*',80);
+	move(0,80);
+	vline('*',25);
+	move(25,0);
+	hline('*',81);
 	for(int i=0;i<149;i++){
 		mvaddch(wall[i][0],wall[i][1],'*');
 	}
@@ -52,6 +36,7 @@ void set_up()
 	for(int i=0;i<5;i++){
 		mvaddch(misson[i][0],misson[i][1],'M');
 	}
+
 	attroff(COLOR_PAIR(1));
 	refresh();
 }
